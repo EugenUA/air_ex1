a2bab5f070bb1529768d2ee353052d3f3f7e00e8
Angelos Arampatzis <angelos.arabatzis@gmail.com>
2018-04-18 23:39:41 +0200

    BM25 scoring

--- src/main/java/Core/BM25VA.java
+++ src/main/java/Core/BM25VA.java
@@ -15,37 +15,26 @@ public class BM25VA {
                                      double documentLength,
                                      double queryFrequency,
                                      double documentFrequency,
-                                     Document document,
-                                     List<Document> documents
+                                     List<Integer> lengthList,
+                                     double averageDocumentLength,
+                                     int corpusSize
     ){
 
         /* Compute b */
-        double averageDocumentLength = getAverageDocumentLength(documents);
         double average_term_frequency = documentLength/averageDocumentLength;
-        double mavgtf = computeMeanAverageTermFrequency(documents);
-        double b_va = (1 / Math.pow(mavgtf,2)) * (documentLength / document.getTerms().size()) +
+        double mavgtf = computeMeanAverageTermFrequency(averageDocumentLength, lengthList, corpusSize);
+        double b_va = (1 / Math.pow(mavgtf,2)) * (documentLength / documentLength) +    //must be number of terms
                 (1 - (1 / mavgtf))*average_term_frequency;
 
         bm25.setB(b_va);
-        return bm25.score(tf,numberOfDocuments,documentLength,getAverageDocumentLength(documents),queryFrequency,documentFrequency);
+        return bm25.score(tf,numberOfDocuments,documentLength,averageDocumentLength,queryFrequency,documentFrequency);
     }
 
-    private double computeMeanAverageTermFrequency(List<Document> documents){
-        double averageDocumentLength = getAverageDocumentLength(documents);
+    private double computeMeanAverageTermFrequency(double averageDocumentLength, List<Integer> lengthList, int corpusSize){
         double sum_avgtf_d = 0;
-        for(Document document : documents){
-            sum_avgtf_d += document.getRawText().length()/averageDocumentLength;
+        for(int length : lengthList) {
+            sum_avgtf_d += length/averageDocumentLength;
         }
-        return sum_avgtf_d / documents.size();
+        return sum_avgtf_d / corpusSize;
     }
-
-    private double getAverageDocumentLength(List<Document> documents){
-        double sumLength = 0;
-        for(Document document : documents){
-            sumLength += document.getRawText().length();
-        }
-        return sumLength / documents.size();
-    }
-
-
 }
